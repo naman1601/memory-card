71c8b68e6cdbcaab01c931055bcbc5f87b5683b4
naman1601 <namanchhaparia1601@gmail.com>
2022-05-27 01:57:59 +0530

    first commit

---
+++ precache-manifest.bda6eebe924fb5e52ac0d9ccb272fb09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b8a67a84f913b6ba44c",
+    "url": "/memory-card/static/css/main.8acb2c48.chunk.css"
+  },
+  {
+    "revision": "4b8a67a84f913b6ba44c",
+    "url": "/memory-card/static/js/main.4b8a67a8.chunk.js"
+  },
+  {
+    "revision": "af2c76b38842c6bebc4b",
+    "url": "/memory-card/static/js/1.af2c76b3.chunk.js"
+  },
+  {
+    "revision": "578ba911c50e8f8ac86f",
+    "url": "/memory-card/static/js/runtime~main.578ba911.js"
+  },
+  {
+    "revision": "07313aa239597913fb9b8c1a68b2d317",
+    "url": "/memory-card/index.html"
+  }
+];
