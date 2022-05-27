7b52e2fee876d46a5f5eb0b1531107a9ae66d5a8
naman1601 <namanchhaparia1601@gmail.com>
2022-05-27 12:04:45 +0530

    downloaded bg image locally instead of using link

---
+++ precache-manifest.105c5f73b90c0d16bf2641ef4d31d84b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfef9819239d13dfdb1d",
+    "url": "/memory-card/static/css/main.f16319dc.chunk.css"
+  },
+  {
+    "revision": "dfef9819239d13dfdb1d",
+    "url": "/memory-card/static/js/main.dfef9819.chunk.js"
+  },
+  {
+    "revision": "af2c76b38842c6bebc4b",
+    "url": "/memory-card/static/js/1.af2c76b3.chunk.js"
+  },
+  {
+    "revision": "578ba911c50e8f8ac86f",
+    "url": "/memory-card/static/js/runtime~main.578ba911.js"
+  },
+  {
+    "revision": "d4ad473f7be24855cca19db25e72eaf4",
+    "url": "/memory-card/static/media/world-map.d4ad473f.webp"
+  },
+  {
+    "revision": "9118f055b472e934ff2fbaa8432e781d",
+    "url": "/memory-card/index.html"
+  }
+];
